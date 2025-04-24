8d40f4dcf07057543388191d4d44ff65b5a09e71
Vicente Fuenzalida <vfuenzalida@buk.cl>
2025-04-24 00:09:15 -0400

    feat: add bill detail

--- src/app/bills/[billId]/page.tsx
+++ src/app/bills/[billId]/page.tsx
@@ -1,4 +1,5 @@
 import { getBillById } from "@/services/bills";
+import { getAllItems } from "@/services/items";
 
 export default async function Bill({
   params,
@@ -7,11 +8,28 @@ export default async function Bill({
 }) {
   const { billId } = await params
   const bill = await getBillById(Number(billId));
+
+  const items = await getAllItems();
+  const billItems = items.filter((item) => item.billId === bill.id);
+  const totalAmount = billItems.reduce((total, item) => {
+    return total + Number(item.unitPrice) * item.quantity;
+  }, 0);
+
   return (
     <div>
       <h1>Bill Details</h1>
       <p>Bill ID: {bill.id}</p>
       <p>Bill Name: {bill.name}</p>
+      <p>Total Amount: {totalAmount}</p>
+      <h2>Items</h2>
+
+      <ul>
+        {billItems.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.unitPrice} x {item.quantity}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
